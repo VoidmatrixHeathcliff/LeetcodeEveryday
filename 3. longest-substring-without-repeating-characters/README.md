8f8b5f18be253ee88d59b18d292796cead2b8372
VoidmatrixHeathcliff <46210663+VoidmatrixHeathcliff@users.noreply.github.com>
2021-03-04 16:43:49 +0800

    4 day

--- 3. longest-substring-without-repeating-characters/README.md
+++ 3. longest-substring-without-repeating-characters/README.md
@@ -4,4 +4,27 @@
 
 今日感悟：***官方题解就是香***
 
+什么？我竟然写出了卡死力扣OJ的代码？  
+（不是卡死，只是判题时间太长了……足足一分多钟）  
+（可能是网络问题，但是难得一见所以记录在下面）
+
+```c++
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> usCharList;
+        int iStrLength = s.size(), iIndexRightPoint = -1, iResult = 0;
+        for(int i = 0; i < iStrLength; i++) {
+            if (i != 0) s.erase(s[i - 1]);
+            while(iIndexRightPoint + 1 < iStrLength && !usCharList.count(s[iIndexRightPoint + 1])) {
+                usCharList.insert(s[iIndexRightPoint + 1]);
+                iIndexRightPoint++;
+            }
+            iResult = max(iResult, iIndexRightPoint - i + 1);
+        }
+        return iResult;
+    }
+};
+```
+
 > 2021 - 03 - 04
--- 3. longest-substring-without-repeating-characters/main.cpp
+++ 3. longest-substring-without-repeating-characters/main.cpp
@@ -1,32 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* pHead = nullptr, *pTail = nullptr;
-        int iCarry = 0;
-        while (l1 || l2) {
-            int iNum_1 = l1 ? l1->val : 0, iNum_2 = l2 ? l2->val : 0;
-            int iSum = iNum_1 + iNum_2 + iCarry;
-            if (!pHead) {
-                pHead = pTail = new ListNode(iSum % 10);
-            } else {
-                pTail->next = new ListNode(iSum % 10);
-                pTail = pTail->next;
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> usCharList;
+        int iStrLength = s.size(), iIndexRightPoint = -1, iResult = 0;
+        for(int i = 0; i < iStrLength; i++) {
+            if (i != 0) usCharList.erase(s[i - 1]);
+            while(iIndexRightPoint + 1 < iStrLength && !usCharList.count(s[iIndexRightPoint + 1])) {
+                usCharList.insert(s[iIndexRightPoint + 1]);
+                iIndexRightPoint++;
             }
-            iCarry = iSum / 10;
-            l1 = l1 ? l1->next : l1;
-            l2 = l2 ? l2->next : l2;
+            iResult = max(iResult, iIndexRightPoint - i + 1);
         }
-        if (iCarry) pTail->next = new ListNode(iCarry);
-        return pHead;
+        return iResult;
     }
 };
