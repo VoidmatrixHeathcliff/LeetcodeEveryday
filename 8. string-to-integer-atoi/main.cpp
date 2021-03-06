85b1c7921e42f6ac46d0b85b5bde6253f22e80d0
VoidmatrixHeathcliff <46210663+VoidmatrixHeathcliff@users.noreply.github.com>
2021-03-06 13:44:53 +0800

    6 day

--- 8. string-to-integer-atoi/main.cpp
+++ 8. string-to-integer-atoi/main.cpp
@@ -2,6 +2,6 @@ class Solution {
 public:
     int myAtoi(string s) {
         long _ltmp = atol(s.c_str());
-        return _ltmp < -2147483648 ? -2147483648 : (_ltmp > 2147483647 ? 2147483647 : _ltmp);
+        return _ltmp < INT_MIN ? INT_MIN : (_ltmp > INT_MAX ? INT_MAX : _ltmp);
     }
 };
