26436cb3280afa055f7bd885940cc82f4907006c
VoidmatrixHeathcliff <46210663+VoidmatrixHeathcliff@users.noreply.github.com>
2021-06-14 23:30:59 +0800

    44 day

---
+++ 剑指 Offer 24. fan-zhuan-lian-biao-lcof/README.md
@@ -0,0 +1,7 @@
+剑指 Offer 24. fan-zhuan-lian-biao-lcof
+
+[原题链接](https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/)
+
+一言难尽，明日加油！
+
+> 2021 - 06 - 14
---
+++ 剑指 Offer 24. fan-zhuan-lian-biao-lcof/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
